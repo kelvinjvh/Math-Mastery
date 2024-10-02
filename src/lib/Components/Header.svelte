6057b8258ab1ba0f77ae56b16067ba7025979512
Kelvin-Joel <joelventura1907@gmail.com>
2024-10-01 21:45:10 -0500

    agregue estilos

--- src/lib/Components/Header.svelte
+++ src/lib/Components/Header.svelte
@@ -1,28 +1,37 @@
 <script>
-    export let showCanvasComponent;
+  export let showCanvasComponent;
 </script>
+
 <div class="header">
-    <div class="pencil" on:click={showCanvasComponent}>✏️</div>
+  <div class="pencil" on:click={showCanvasComponent}>✏️</div>
+</div>
+
+<div class="elementores">
+  <div class="protocolos">
+    <div class="diamantes">
+      
+    </div>
+  </div>
 </div>
 
 <style>
-    .header{
-        width: 90%;
-        display: flex;
-        align-items: flex-end;
-        justify-content: flex-end;
-        font-size: 1.5em;
-        margin: 1em auto;
-    }
-    .pencil{
-        border-radius: 50%;
-        height: 40px;
-        width: 40px;
-        background-color: #fff;
-        display: grid;
-        place-items: center;
-        font-size: .9em;
-        cursor: pointer;
-        box-shadow: 0 0 5px #fff;
-    }
+  .header {
+    width: 90%;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
+    font-size: 1.5em;
+    margin: 1em auto;
+  }
+  .pencil {
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+    background-color: #fff;
+    display: grid;
+    place-items: center;
+    font-size: 0.9em;
+    cursor: pointer;
+    box-shadow: 0 0 5px #fff;
+  }
 </style>
--- src/lib/Components/OperationsMathComponet.svelte
+++ src/lib/Components/OperationsMathComponet.svelte
@@ -12,18 +12,27 @@
 <h2 class="title">Operaciones Matemáticas!!</h2>
 <div class="operation-container">
   {#each ListOperationsMath as mathOperation}
-    {#if operationSymbol === "➖" || operationSymbol === "✖" && mathOperation.n1 < mathOperation.n2}
+    {#if operationSymbol === "➖" || (operationSymbol === "✖" && mathOperation.n1 < mathOperation.n2)}
       <div class="operation-item">
-        <p>{mathOperation.n2} <span>{operationSymbol}</span> {mathOperation.n1}</p>
+        <p>
+          {mathOperation.n2} <span>{operationSymbol}</span>
+          {mathOperation.n1}
+        </p>
         =<input
           type="number"
           bind:value={mathOperation.answer}
           on:input={() => AnswerVerification(mathOperation)}
-        /> <span>{mathOperation.feedback === "" ? "❓" : mathOperation.feedback}</span>
+        />
+        <span
+          >{mathOperation.feedback === "" ? "❓" : mathOperation.feedback}</span
+        >
       </div>
     {:else}
       <div class="operation-item">
-        <p>{mathOperation.n1} <span>{operationSymbol}</span> {mathOperation.n2}</p>
+        <p>
+          {mathOperation.n1} <span>{operationSymbol}</span>
+          {mathOperation.n2}
+        </p>
         =<input
           type="number"
           bind:value={mathOperation.answer}
@@ -43,78 +52,77 @@
 
 <style>
   .title {
-  text-align: center;
-  font-size: 1.5em;
-  margin-bottom: 1em;
-  color: #fff; /* Asegúrate de que el color del texto sea legible */
-}
+    text-align: center;
+    font-size: 1.5em;
+    margin-bottom: 1em;
+    color: #fff; /* Asegúrate de que el color del texto sea legible */
+  }
 
-.operation-container {
-  width: 100%;
-  margin-bottom: 1em;
-  border-radius: 5px;
-  color: #fff;
-  display: grid;
-  place-items: center;
-}
+  .operation-container {
+    width: 100%;
+    margin-bottom: 1em;
+    border-radius: 5px;
+    color: #fff;
+    display: grid;
+    place-items: center;
+  }
 
-.operation-item {
-  font-size: 1.8em;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  margin-bottom: 0.4em;
-  width: 100%;
-  border-radius: 3px;
-  padding: 0.3em;
-}
+  .operation-item {
+    font-size: 1.8em;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 0.4em;
+    width: 100%;
+    border-radius: 3px;
+    padding: 0.3em;
+  }
 
-.operation-item p {
-  margin: 0;
-  width: 150px;
-  font-weight: bold;
-}
+  .operation-item p {
+    margin: 0;
+    width: 150px;
+    font-weight: bold;
+  }
 
-.operation-item span {
-  font-size: 0.7em;
-}
+  .operation-item span {
+    font-size: 0.7em;
+  }
 
-input {
-  width: 70px;
-  height: 40px;
-  border: none;
-  outline: none;
-  background-color: #fff;
-  border-radius: 5px;
-  text-align: center;
-  color: #111;
-  margin-left: 0.7em;
-  font-size: 0.8em;
-  font-weight: bold;
-}
+  input {
+    width: 70px;
+    height: 40px;
+    border: none;
+    outline: none;
+    background-color: #fff;
+    border-radius: 5px;
+    text-align: center;
+    color: #111;
+    margin-left: 0.7em;
+    font-size: 0.8em;
+    font-weight: bold;
+  }
 
-.feedback {
-  width: 20px;
-  font-size: 1em;
-}
+  .feedback {
+    width: 20px;
+    font-size: 1em;
+  }
 
-.button-container {
-  width: 100%;
-  display: flex;
-  justify-content: space-evenly; /* Distribuye los botones con espacio entre ellos */
-}
+  .button-container {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly; /* Distribuye los botones con espacio entre ellos */
+  }
 
-button {
-  background-color: var(--secondary-color);
-  color: #fff;
-  font-weight: bold;
-  display: inline-block;
-  font-size: 1.2em;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  border: none; /* Añadido para eliminar borde predeterminado */
-  border-radius: 5px; /* Bordes redondeados para los botones */
-  padding: 0.5em 1em; /* Padding para los botones */
-  cursor: pointer; /* Cambia el cursor al pasar sobre el botón */
-}
-
+  button {
+    background-color: var(--secondary-color);
+    color: #fff;
+    font-weight: bold;
+    display: inline-block;
+    font-size: 1.2em;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    border: none; /* Añadido para eliminar borde predeterminado */
+    border-radius: 5px; /* Bordes redondeados para los botones */
+    padding: 0.5em 1em; /* Padding para los botones */
+    cursor: pointer; /* Cambia el cursor al pasar sobre el botón */
+  }
 </style>
